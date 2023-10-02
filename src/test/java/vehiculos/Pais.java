0e072fcc0626818b25af8e6649b467228359902f
Mnico-11 <141962291+Mnico-11@users.noreply.github.com>
2023-10-01 23:07:35 -0500

    ojalaaa

---
+++ src/test/java/vehiculos/Pais.java
@@ -0,0 +1,68 @@
+package vehiculos;
+
+
+import java.util.List;
+
+import java.util.ArrayList;
+
+public class Pais {
+	
+	private String nombre;
+	private int cantidadVehiculos;
+	private static List<Pais> paises = new ArrayList<Pais>();
+	
+	public Pais(String nombre) {
+		
+		this.nombre = nombre;
+		
+		Pais.paises.add(this);
+		
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getCantidadVehiculos() {
+		return cantidadVehiculos;
+	}
+
+	public void setCantidadVehiculos(int cantidadVehiculos) {
+		this.cantidadVehiculos = cantidadVehiculos;
+	}
+
+	public static List<Pais> getPaises() {
+		return paises;
+	}
+
+	public static void setPaises(List<Pais> paises) {
+		Pais.paises = paises;
+	}
+	
+    public static Pais paisMasVendedor() {
+		
+		int max = 0;
+		Pais vaa = null;
+		
+		for (int i = 0; i<Pais.paises.size(); i++){
+			
+			int x = Pais.paises.get(i).cantidadVehiculos;
+			
+			if (max < x) {
+				
+				max = x;
+				vaa = Pais.paises.get(i);
+	
+			}
+			
+		}
+		
+		return vaa;
+		
+	}
+
+}
